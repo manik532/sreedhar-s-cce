98942d68b1a346e87dfe803a26069fb2525074a8
manik532 <maniloveforyou@gmail.com>
2020-12-20 03:14:34 +0530

    SquareNumber

---
+++ main.py
@@ -0,0 +1,12 @@
+Number=int(input("Enter YOur Number to Check:- "))
+def Check(N):
+    if N%2==0:
+        print("Your Number",N,"is Even Number.")
+    else:
+        print("Your Number",N," is Odd Number.")
+
+def square_(N)
+    R=N*N
+    print("Square of",N,"is ",R)
+Check(Number)
+square_(Number)
